60dd80bdaf94ac2729b8b24403ce59f9eff3e1e5
Ikuo Terado <eqobar@gmail.com>
2020-09-05 02:58:25 +0900

    Minor changes

--- src/index.ts
+++ src/index.ts
@@ -28,10 +28,30 @@ class PhysicsExtension {
       this[functionName] = this.blocks.functions[functionName].bind(this)
     }
 
-    const canvas = Utils.getCanvasForPhysics(Scratch.Canvas.WIDTH, Scratch.Canvas.HEIGHT)
+    const canvas = this.getCanvasForPhysics(Scratch.Canvas.WIDTH, Scratch.Canvas.HEIGHT)
     const physics = new Physics(canvas, { isVisible: Utils.isDebug() })
 
     this.targets = new Targets(this.runtime, physics)
+  }
+
+  private getCanvasForPhysics(width: number, height: number): HTMLCanvasElement {
+    const element = document.querySelector('canvas.physics') as HTMLCanvasElement
+    if (element) {
+      return element
+    }
+
+    const canvas = document.createElement('canvas')
+    canvas.className = 'physics'
+    canvas.width = width
+    canvas.height = height
+    canvas.style.zIndex = '100'
+    canvas.style.position = 'absolute'
+    canvas.style.bottom = '0px'
+    canvas.style.left = '320px'
+
+    document.body.prepend(canvas)
+
+    return canvas
   }
 
   getInfo() {
